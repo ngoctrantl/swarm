f351367dc793f3d46d47d723de313a5cdb68dc92
acud <theman@elad.im>
2019-07-01 09:24:49 +0300

    wip stream abstraction

---
+++ network/streem/streem.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Swarm Authors
+// This file is part of the Swarm library.
+//
+// The Swarm library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Swarm library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Swarm library. If not, see <http://www.gnu.org/licenses/>.
+
+package streem
+
+import "github.com/ethersphere/swarm/p2p/protocols"
+
+var SyncerSpec = &protocols.Spec{
+	Name:       "streem",
+	Version:    8,
+	MaxMsgSize: 10 * 1024 * 1024,
+	Messages: []interface{}{
+		StreamInfoReq{},
+		StreamInfoRes{},
+		GetRange{},
+		OfferedHashes{},
+		ChunkDelivery{},
+		WantedHashes{},
+	},
+}
